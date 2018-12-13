ff46c19c7066aedab1e768da4eca4fc169b4afd4
Ewa Jedrzejowska <efciajed@gmail.com>
2018-12-13 10:07:50 +0100

    di-demo update

---
+++ di-demo/src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -0,0 +1,19 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+    private GreetingService greetingService;
+
+    @Autowired //Autowired not required for constructor based DI
+    public ConstructorInjectedController(GreetingService greetingService){
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
